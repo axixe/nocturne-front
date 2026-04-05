4271eea3b8bcd29e03754cb7df647e133dabfd39
axixe <s1ngleez@yandex.ru>
2026-04-05 22:23:47 +0300

    feat(nuxt.config): update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/scss/main.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/mixins.scss";
+                           @import "@/assets/scss/variables.scss";`,
+        },
+      },
+    },
+    base: '/'
+  },
 })
